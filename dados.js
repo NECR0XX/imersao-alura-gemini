eef54cd348632658367b679fce2173f384539cb4
NECR0XX <carronananiasx@gmail.com>
2024-09-04 19:14:25 -0300

    Aula 3: CRIANDO A BASE DE DADOS COM JAVASCRIPT

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Química Inorgânica",
+        descricao: "A Química Inorgânica é a área da ciência que estuda os compostos formados por todos os elementos da tabela periódica, com exceção dos compostos orgânicos (que contêm carbono ligado a hidrogênio). Ela explora as propriedades, estruturas e reações de substâncias presentes na natureza e em materiais criados pelo homem.",
+        link: "https://www.todamateria.com.br/quimica-inorganica/"
+    },
+    {
+        titulo: "Química Orgânica",
+        descricao: "A Química Orgânica é o ramo da química que estuda os compostos que contêm carbono em sua estrutura molecular. Ela é fundamental para o desenvolvimento de medicamentos, plásticos, tintas e muitos outros produtos.",
+        link: "https://www.todamateria.com.br/quimica-organica/"
+    },
+    {
+        titulo: "Química Analítica",
+        descricao: "A Química Analítica é a área da química que desenvolve instrumentos e métodos para separação, identificação e quantificação de matéria.",
+        link: "https://www.infoescola.com/quimica/quimica-analitica/"
+    },
+];
