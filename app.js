e67498cb64f48465769bec91babfae14f31b0684
NECR0XX <carronananiasx@gmail.com>
2024-09-06 23:00:08 -0300

    Aula 5:FINALIZANDO O PROJETO COM GITHUB E DEPLOY

--- app.js
+++ app.js
@@ -2,18 +2,38 @@ function pesquisar() {
     
 let section = document.getElementById('resultados-pesquisa');
 
+let campoPesquisa = document.getElementById('campo-pesquisa').value;
+
+if (!campoPesquisa) {
+    section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar algo relacionado a química.</p>";
+    return 
+} 
+
+campoPesquisa = campoPesquisa.toLowerCase();
+
 let resultados = "";
+let titulo = "";
+let descricao = "";
+let tags = "";
 
 for (let dado of dados) {
-    resultados += `
-    <div class="item-resultado">
-        <h2>
-            <a href="#" target="_blank" rel="noopener noreferrer">${dado.titulo}</a>
-        </h2>
-        <p class="descricao-meta">${dado.descricao}</p>
-        <a href="${dado.link}" target="_blank" rel="noopener noreferrer">Mais Informações</a>
-    </div>
-`
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        resultados += `
+        <div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank" rel="noopener noreferrer">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank" rel="noopener noreferrer">Mais Informações</a>
+        </div>
+    `
+    } else {
+        section.innerHTML = "<p>Nenhum conteúdo foi encontrado.</p>";
+        return
+    }
 }
 
 section.innerHTML = resultados;
--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let dados = [
     {
         titulo: "Química Inorgânica",
         descricao: "A Química Inorgânica é a área da ciência que estuda os compostos formados por todos os elementos da tabela periódica, com exceção dos compostos orgânicos (que contêm carbono ligado a hidrogênio). Ela explora as propriedades, estruturas e reações de substâncias presentes na natureza e em materiais criados pelo homem.",
-        link: "https://www.todamateria.com.br/quimica-inorganica/"
+        link: "https://www.todamateria.com.br/quimica-inorganica/",
+        tags: "inorganica acidos ácidos base bases sais sal óxidos oxidos oxido"
     },
     {
         titulo: "Química Orgânica",
         descricao: "A Química Orgânica é o ramo da química que estuda os compostos que contêm carbono em sua estrutura molecular. Ela é fundamental para o desenvolvimento de medicamentos, plásticos, tintas e muitos outros produtos.",
-        link: "https://www.todamateria.com.br/quimica-organica/"
+        link: "https://www.todamateria.com.br/quimica-organica/",
+        tags: "organica carbono"
     },
     {
         titulo: "Química Analítica",
         descricao: "A Química Analítica é a área da química que desenvolve instrumentos e métodos para separação, identificação e quantificação de matéria.",
-        link: "https://www.infoescola.com/quimica/quimica-analitica/"
+        link: "https://www.infoescola.com/quimica/quimica-analitica/",
+        tags: "analitica"
     },
 ];
